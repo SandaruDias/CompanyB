4ab1e11cc53581b04ab8efa056b8c7026270ba4a
Sandaru Dias <162311144+SandaruDias@users.noreply.github.com>
2024-04-12 13:50:24 +0530

    Add files via upload

---
+++ CompanyB/CompanyB/src/main/java/com/example/CompanyB/CompanyBApplication.java
@@ -0,0 +1,13 @@
+package com.example.CompanyB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyBApplication.class, args);
+	}
+
+}
---
+++ CompanyB/CompanyB/src/test/java/com/example/CompanyB/CompanyBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CompanyB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
