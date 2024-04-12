b16919f2124be23fb3e332b9afe5d5dbd9d6cff1
Sandaru Dias <162311144+SandaruDias@users.noreply.github.com>
2024-04-12 15:09:49 +0530

    Update SimulationController.java

--- TrainingSimulationPrototypingModule/Controller/SimulationController.java
+++ TrainingSimulationPrototypingModule/Controller/SimulationController.java
@@ -5,5 +5,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // Make SimulationController a controller component
 public class SimulationController
 {
-
+  private int cid;
 }
