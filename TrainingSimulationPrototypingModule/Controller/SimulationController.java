a39269fcc1bab4de24476c1f3857e63fe5b8e1fb
Sandaru Dias <162311144+SandaruDias@users.noreply.github.com>
2024-04-12 15:16:53 +0530

    Revert "Training/simulation/prototyping"

--- TrainingSimulationPrototypingModule/Controller/SimulationController.java
+++
@@ -1,9 +0,0 @@
-package com.example.CompanyB.TrainingSimulationPrototypingModule.Controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController // Make SimulationController a controller component
-public class SimulationController
-{
-  private int cid;
-}
--- TrainingSimulationPrototypingModule/Service/SampleService3.java
+++
@@ -1,4 +0,0 @@
-package com.example.CompanyB.TrainingSimulationPrototypingModule.Service;
-
-public class SampleService3 {
-}
