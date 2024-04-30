afc8c45dde7b6756d419f573281a5a70dd70633b
Prabath Wijethilaka <121922040+PrabathBK@users.noreply.github.com>
2024-05-01 02:13:53 +0530

    Delelte Unwated FrontEnd files and add wanted version

--- ManufacturingFrontEnd/src/components/index.jsx
+++
@@ -1,6 +0,0 @@
-import { Text } from "./Text";
-import { Heading } from "./Heading";
-import { Img } from "./Img";
-import { Button } from "./Button";
-import { Input } from "./Input";
-export { Text, Heading, Img, Button, Input };
