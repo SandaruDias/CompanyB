6719e16e9af0ffe4c6b206f1f56fbef3a003d219
Prabath Wijethilaka <121922040+PrabathBK@users.noreply.github.com>
2024-05-01 03:49:14 +0530

    Admin page fetch done

--- CompanyB/src/main/java/com/example/CompanyB/ManufacturingModule/Controller/OnGoingOrderController.java
+++ CompanyB/src/main/java/com/example/CompanyB/ManufacturingModule/Controller/OnGoingOrderController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("http://localhost:5173/")
 @RequestMapping("/OnGoingOrder")
 
 public class OnGoingOrderController {
--- CompanyB/src/main/java/com/example/CompanyB/ManufacturingModule/Controller/StationFetchController.java
+++ CompanyB/src/main/java/com/example/CompanyB/ManufacturingModule/Controller/StationFetchController.java
@@ -5,12 +5,10 @@ import com.example.CompanyB.ManufacturingModule.Service.StationFetchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("http://localhost:5173/")
 @RequestMapping("/StationFetch")
 public class StationFetchController {
 
