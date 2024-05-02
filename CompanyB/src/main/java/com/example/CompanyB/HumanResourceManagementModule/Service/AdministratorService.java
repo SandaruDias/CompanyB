b7c9cb26b2e0032af02d178379f3bcde9178c42d
prathibhainf <166288901+prathibhainf@users.noreply.github.com>
2024-05-02 21:16:53 +0530

    updated_attendance_tracking
    administrator class is also updated

--- CompanyB/src/main/java/com/example/CompanyB/HumanResourceManagementModule/Service/AdministratorService.java
+++ CompanyB/src/main/java/com/example/CompanyB/HumanResourceManagementModule/Service/AdministratorService.java
@@ -2,17 +2,23 @@ package com.example.CompanyB.HumanResourceManagementModule.Service;
 
 import com.example.CompanyB.HumanResourceManagementModule.Model.Administrator;
 import com.example.CompanyB.HumanResourceManagementModule.Model.Employee;
+import com.example.CompanyB.HumanResourceManagementModule.Model.EmployeeAttendanceModel;
 import com.example.CompanyB.HumanResourceManagementModule.Repository.AdministratorRepo;
+import com.example.CompanyB.HumanResourceManagementModule.Repository.EmployeeAttendanceRepo;
+import com.example.CompanyB.HumanResourceManagementModule.Repository.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdministratorService {
 
     @Autowired
     private AdministratorRepo administratorRepo;
+    @Autowired
+    private EmployeeAttendanceRepo employeeAttendanceRepo;
 
     public Administrator getAdministratorById(String id) {
         return administratorRepo.findById(id).orElse(null);
@@ -31,6 +37,11 @@ public class AdministratorService {
 
     public void changePassword(String id,String newPassword){
         administratorRepo.findById(id).get().setPassword(newPassword);
+    }
+    public boolean getPermission(String id){
+        Optional<EmployeeAttendanceModel> employeeAttendanceModel = employeeAttendanceRepo.findById(id);
+        employeeAttendanceModel.get().setShortLeave("Taken");
+        return true;
     }
 
 }
