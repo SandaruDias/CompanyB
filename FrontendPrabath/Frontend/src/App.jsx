ae57255ea6815c81271ed80d76f1b2ea2c286d4d
Prabath Wijethilaka <121922040+PrabathBK@users.noreply.github.com>
2024-05-05 10:44:06 +0530

    change app.jsx

--- FrontendPrabath/Frontend/src/App.jsx
+++ FrontendPrabath/Frontend/src/App.jsx
@@ -20,17 +20,17 @@ function App() {
     <Router>
       <Switch>
       
-        <Route path="companyB/manufacturing/" exact component={SelectionPage} />
-        <Route path="companyB/manufacturing/adminPortal" component={LoginPage} />
-        <Route path="companyB/manufacturing/workerPortal" component={WorkerPortal} />
-        <Route path="companyB/manufacturing/StockManag" component={StockManag} />
-        <Route path="companyB/manufacturing/AddItem" component={AddItem} />
-        <Route path="companyB/manufacturing/generatereport" component={GenerateReport} />
-        <Route path="companyB/manufacturing/updatestock" component={UpdateStock} />
-        <Route path="companyB/manufacturing/AdminInterface" component={admininterface}/>
-        <Route path="companyB/manufacturing/WorkStationOne" component={WorkerInterfaceOne} />
-        <Route path="companyB/manufacturing/WorkStationTwo" component={WorkerInterfaceTwo} />
-        <Route path="companyB/manufacturing/WorkStationThree" component={WorkerInterfaceThree} />
+        <Route path="/" exact component={SelectionPage} />
+        <Route path="/companyB/manufacturing/adminPortal" component={LoginPage} />
+        <Route path="/companyB/manufacturing/workerPortal" component={WorkerPortal} />
+        <Route path="/companyB/manufacturing/StockManag" component={StockManag} />
+        <Route path="/companyB/manufacturing/AddItem" component={AddItem} />
+        <Route path="/companyB/manufacturing/generatereport" component={GenerateReport} />
+        <Route path="/companyB/manufacturing/updatestock" component={UpdateStock} />
+        <Route path="/companyB/manufacturing/AdminInterface" component={admininterface}/>
+        <Route path="/companyB/manufacturing/WorkStationOne" component={WorkerInterfaceOne} />
+        <Route path="/companyB/manufacturing/WorkStationTwo" component={WorkerInterfaceTwo} />
+        <Route path="/companyB/manufacturing/WorkStationThree" component={WorkerInterfaceThree} />
 
       </Switch>
     </Router>
