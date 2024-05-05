8ac68de6dca1c854189ec44d60699e0106180640
Nimesh Lal <nimeshlal097@gmail.com>
2024-05-05 08:16:33 +0530

    new a simul

--- CompanyB/src/main/java/com/example/CompanyB/TrainingSimulationPrototypingModule/Model/SimTest.java
+++ CompanyB/src/main/java/com/example/CompanyB/TrainingSimulationPrototypingModule/Model/SimTest.java
@@ -2,13 +2,9 @@ package com.example.CompanyB.TrainingSimulationPrototypingModule.Model;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-
-import java.awt.*;
 
 @Document(collection = "Simulations")
 @Data
@@ -27,4 +23,21 @@ public class SimTest
     private boolean thermalSimulStatus;
     @Field("Manufacturability")
     private boolean manufacturabilityStatus;
+
+
+    public SimTest() {
+        // Default constructor
+    }
+
+    public SimTest(String testID, boolean simulApproval, String simulComments,
+                   boolean circuitSimulStatus, boolean thermalSimulStatus, boolean manufacturabilityStatus) {
+        this.testID = testID;
+        this.simulApproval = simulApproval;
+        this.simulComments = simulComments;
+        this.circuitSimulStatus = circuitSimulStatus;
+        this.thermalSimulStatus = thermalSimulStatus;
+        this.manufacturabilityStatus = manufacturabilityStatus;
+    }
+
+
 }
