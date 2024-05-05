399c65976f3a2a5de17b19425974f9efec39d45a
prathibhainf <166288901+prathibhainf@users.noreply.github.com>
2024-05-05 11:08:42 +0530

    Test cases added for attendance tracking

---
+++ CompanyB/src/test/java/com/example/CompanyB/HumanResourceManagementModule/Controller/EmployeeAttendanceControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.CompanyB.HumanResourceManagementModule.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeAttendanceControllerTest {
+
+    @Test
+    void checkIn() {
+    }
+
+    @Test
+    void checkOut() {
+    }
+}
---
+++ CompanyB/src/test/java/com/example/CompanyB/HumanResourceManagementModule/Service/EmployeeAttendanceServiceTest.java
@@ -0,0 +1,32 @@
+package com.example.CompanyB.HumanResourceManagementModule.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeAttendanceServiceTest {
+
+    @Test
+    void isValidId() {
+    }
+
+    @Test
+    void isValidIdCheckout() {
+    }
+
+    @Test
+    void isValidCheckInTime() {
+    }
+
+    @Test
+    void isValidCheckOutTime() {
+    }
+
+    @Test
+    void recordCheckIn() {
+    }
+
+    @Test
+    void recordCheckOut() {
+    }
+}
